13ee6d7e6ca2e4074044b9c1105facf033bd4010
Artem Bolotov <teeemon@gmail.com>
2023-03-07 07:27:25 +0300

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by artembolotov on 07.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
